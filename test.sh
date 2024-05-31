c49ad24ffa040c64d4c5de5acf8c7c8a2960b03d
Shane Poppleton <shane@bjja.com.au>
2024-05-31 17:45:16 +1000

    Added running of test.sh to CI

--- test.sh
+++ test.sh
@@ -2,17 +2,13 @@
 
 DIRECTORY=test-dir
 
-if [ -d $DIRECTORY ]; then
-    echo "Directory $DIRECTORY already exists"
-    exit 1
-fi
+rm -rf $DIRECTORY
 
 gleam run -- --target-dir $DIRECTORY
 
 if [ ! -d $DIRECTORY ]; then
-    echo "Directory $DIRECTORY was not created"
-    exit 1
+	echo "Directory $DIRECTORY was not created"
+	exit 1
 fi
 
-rm -rf $DIRECTORY
 echo "Test passed!"
